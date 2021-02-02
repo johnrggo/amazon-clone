f734c3cedc0b14af3a65a5ce8ddc20049d25efc7
John Regner Go <62073044+reggae-shark-007@users.noreply.github.com>
2021-02-02 09:47:11 +0800

    Day 2 amazon-clone challenge. Added home and checkout page.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBpTzFrQGCXa9UGTbLNtQoj_t4WF4JG--E",
+    authDomain: "challenge-b05e8.firebaseapp.com",
+    projectId: "challenge-b05e8",
+    storageBucket: "challenge-b05e8.appspot.com",
+    messagingSenderId: "323456490147",
+    appId: "1:323456490147:web:94280524db599d3a973d16",
+    measurementId: "G-QDKNWFZV7E"
+  };
