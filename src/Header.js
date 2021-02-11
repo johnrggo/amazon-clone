1807451033a5bec7c4522e3b2fe68b1201632527
John Regner Go <62073044+reggae-shark-007@users.noreply.github.com>
2021-02-11 21:17:15 +0800

    Completed the checkout page and user authentication.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+
   return (
     <div className="header">
       <Link to="/">
@@ -16,15 +27,23 @@ function Header() {
       </Link>
 
       <div className="header__search">
-        <input className="header__searchInput" type="text" />
+        <input
+          className="header__searchInput"
+          type="text"
+          placeholder="Search for Products"
+        />
         <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineTwo">Sign In</span>
-        </div>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
@@ -39,7 +58,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBpTzFrQGCXa9UGTbLNtQoj_t4WF4JG--E",
-    authDomain: "challenge-b05e8.firebaseapp.com",
-    projectId: "challenge-b05e8",
-    storageBucket: "challenge-b05e8.appspot.com",
-    messagingSenderId: "323456490147",
-    appId: "1:323456490147:web:94280524db599d3a973d16",
-    measurementId: "G-QDKNWFZV7E"
-  };
+  apiKey: "AIzaSyBpTzFrQGCXa9UGTbLNtQoj_t4WF4JG--E",
+  authDomain: "challenge-b05e8.firebaseapp.com",
+  projectId: "challenge-b05e8",
+  storageBucket: "challenge-b05e8.appspot.com",
+  messagingSenderId: "323456490147",
+  appId: "1:323456490147:web:94280524db599d3a973d16",
+  measurementId: "G-QDKNWFZV7E",
+};
+
+// initilize firebase
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
